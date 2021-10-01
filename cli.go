b22f25869e962b72e7b58b3a0b9a5eaa330ddcf0
Yusuke Wada <yusuke@kamawada.com>
2021-10-01 10:00:26 +0900

    Show Usage

--- cli.go
+++ cli.go
@@ -1,6 +1,7 @@
 package emojify
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 
@@ -15,7 +16,8 @@ func (cli *CLI) Run() {
 	var text string
 	if terminal.IsTerminal(0) {
 		if len(os.Args) <= 1 {
-			os.Exit(1)
+			cli.showUsage()
+			os.Exit(0)
 		}
 		text = os.Args[1]
 		text = text + "\n"
@@ -24,4 +26,8 @@ func (cli *CLI) Run() {
 		text = string(b)
 	}
 	emoji.Print(text)
+}
+
+func (cli *CLI) showUsage() {
+	fmt.Println("Usage: $ go-emojify \"I love :ramen:\"")
 }
